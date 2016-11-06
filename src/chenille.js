679d06b893649e93664b5eff5bea4d3648e43fac
Francois Best <francoisbest@gmail.com>
2016-11-06 09:35:43 +0100

    Fixed Chenille init and math functions.

--- src/chenille.js
+++ src/chenille.js
@@ -1,6 +1,6 @@
-//* 
+//*
 /*   Script définissant permettant l'animation d'objets 'Chenille' à l'intérieur d'un canvas
- * 
+ *
  * @param {type} xInit
  * @param {type} yInit
  * @param {type} rInit
@@ -25,7 +25,7 @@ function Anneau(xInit, yInit, rInit) {
     this.rInit = rInit;
 }
 ;
-/** 
+/**
  * positionne le centre de l'anneau en un point donné
  * @param {integer} px abscisse du point
  * @param {integer} py ordonnée du point
@@ -72,13 +72,13 @@ Tete.prototype.devierCap = function (deltaC) {
 };
 
 Tete.prototype.deplacerSelonCap = function () {
-    this.x = this.x + (this.r * cos(this.cap));
-    this.y = this.y + (this.r * sin(this.cap));
+    this.x = this.x + (this.r * Math.cos(this.cap));
+    this.y = this.y + (this.r * Math.sin(this.cap));
 };
 
-// qui retourne un booléen dont la valeur est vraie (true) si le cap actuel 
-// de la tête est tel que le prochain déplacement maintiendra la tête entièrement 
-// dans le canvas et faux (false) sinon. Plus précisément, cette méthode vérifie 
+// qui retourne un booléen dont la valeur est vraie (true) si le cap actuel
+// de la tête est tel que le prochain déplacement maintiendra la tête entièrement
+// dans le canvas et faux (false) sinon. Plus précisément, cette méthode vérifie
 // que le point (x',y') défini par x' = x + R * cos(cap) et y' = y + R * sin(cap)
 // est à une distance >= R de chacun des bords du canvas.
 
@@ -87,68 +87,72 @@ Tete.prototype.capOK = function (canvas) {
     var xmax=canvas.width-this.r;
     var ymin=0+this.r;
     var ymax=canvas.height-this.r;
-    var newx=this.x + (this.r * cos(this.cap));
-    var newy=this.y + (this.r * sin(this.cap));
+    var newx=this.x + (this.r * Math.cos(this.cap));
+    var newy=this.y + (this.r * Math.sin(this.cap));
     if (newx>=xmin && newx<=xmax && newy>=ymin && newy<=ymax){
         return true;
     }else{
         return false;
-    };    
+    };
 };
 
 function Chenille(canvas, nbAnneaux, r) {
     this.canvas = canvas;
     this.nbAnneaux = nbAnneaux;
     this.r = r;
-}
-;
-Chenille.prototype.dessiner = function () {
+    this.head = null;
+    this.corps = null;
+};
+Chenille.prototype.init = function() {
     var ctxt = this.canvas.getContext("2d");
     this.head = new Tete(this.canvas.width / 2, this.canvas.height/2, this.r, 0);
-    this.head.dessiner(ctxt);
-    anneaux=[];
-    for (i=0; i<this.nbAnneaux;i++){
-        var cetAnneau= new Anneau((this.canvas.width/2)-(i+1)*this.r, this.canvas.height/2, this.r);
-        anneaux.push=cetAnneau;
-        cetAnneau.dessiner(ctxt);
+    this.corps=[];
+    for (var i = 0; i < this.nbAnneaux; i++) {
+        this.corps.push(new Anneau((this.canvas.width/2)-(i+1)*this.r, this.canvas.height/2, this.r));
     };
-    this.corps=anneaux;
-    
+}
 
+Chenille.prototype.dessiner = function () {
+    var ctxt = this.canvas.getContext("2d");
+    this.head.dessiner(ctxt);
+    for (var i=0; i<this.nbAnneaux;i++){
+        this.corps[i].dessiner(ctxt);
+    };
 };
 Chenille.prototype.deplacer = function () {
     // move body first starting from the tail end
-    for (i=this.nbAnneaux-1; i>0; i--){
+    for (var i=this.nbAnneaux-1; i>0; i--){
         var cetAnneau=this.corps[i];
         var nextAnneau=this.corps[i-1];
         var newx=nextAnneau.xInit;
         var newy=nextAnneau.yInit;
-        cetAnneau.placerA(newx,newy);    
+        cetAnneau.placerA(newx,newy);
     }
     // move first anneau to head position
     var newx=this.head.x;
     var newy=this.head.y;
-    this.corps[0].placerA(newx,newy); 
+    this.corps[0].placerA(newx,newy);
     //move head
     var deltaC=getRandomIntInclusive(-30, 30);
     this.head.devierCap(deltaC);
-    while (!this.head.capOK(canvas)) {
+    while (!this.head.capOK(this.canvas)) {
         this.head.devierCap(10);
     };
-    this.head.deplacerseloncap();
+    this.head.deplacerSelonCap();
 };
 
 function init() {
     var timerId = 0;
     var canvas = document.getElementById("myCanvas");
     var ctxt = canvas.getContext("2d");
-    
+
     // create the worms
-    
+
     var bug1 = new Chenille(canvas, 10,10);
+    bug1.init();
     bug1.dessiner();
-    
-    
+
+
     // association au bouton Start d'un traitement qui lance l'animation
     document.getElementById("startBtn").onclick = function() {
         // change l'état des boutons Start et Stop
@@ -163,7 +167,7 @@ function init() {
             bug1.dessiner();
         }, 20);
     };
-    
+
      // association au bouton Stop d'un traitement qui interrompt l'animation
     document.getElementById("stopBtn").onclick = function() {
         // change l'état des boutons Start et Stop
